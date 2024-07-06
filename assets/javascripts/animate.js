0173be689a8e26ac13ac1937db02adc0422a2bad
luongdev1 <luongdev1601@gmail.com>
2024-07-06 14:56:06 +0700

    done-desktop-ui-ux

--- assets/javascripts/animate.js
+++ assets/javascripts/animate.js
@@ -1,99 +1,95 @@
 /*
-     *animate showItemDownToUp down to up
-
+     *handle add animate
+     *page blog 1
+     *page blog details
+     *wait dom
 */
-// animate showItemDownToUp down to up
-function checkScrollPosition() {
-     // Chieu cao cua cua so trinh duyet
-     var windowHeight = $(window).height();
-     // vi tri scroll hien tai cua cua so
-     var scrollPosition = $(window).scrollTop();
-     // animate cho blog item page blog
-     $(".blog-item").each(function () {
-          // vi tri hien tai cua element
-          const elementTop = $(this).offset().top;
-          if (scrollPosition + windowHeight > elementTop + 50) {
-               $(this).addClass("showItemDownToUp").removeClass("hidden");
-          }
-     });
 
-     // animate cho logo footer
-     const logoFooter = $(".footer-heading .logo")
-     const logoFooterElementTop = logoFooter.offset().top
-     if (scrollPosition + windowHeight > logoFooterElementTop) {
-          $(logoFooter).addClass("showItemDownToUp").removeClass("hidden")
-     }
-     // animate cho icon social
-     $(".footer-social li").each(function () {
-          const elementTop = $(this).offset().top;
-          if (scrollPosition + windowHeight > elementTop) {
-               $(this).addClass("showItemDownToUp").removeClass("hidden")
-          }
-     })
-     // animate footer infor
-     $(".footer-information ul").each(function () {
-          const elementTop = $(this).offset().top;
-          if (scrollPosition + windowHeight > elementTop) {
-               $(this).addClass("showItemDownToUp").removeClass("hidden")
-          }
-     })
-     // animate cho send mail footer
-     const sendMail = $(".footer-sendMail")
-     const sendMailElTop = logoFooter.offset().top
-     if (scrollPosition + windowHeight > sendMailElTop) {
-          $(sendMail).addClass("showItemDownToUp").removeClass("hidden")
-     }
-}
-
-function animateBlogDetails() {
+//function Add animate
+function handleAddAnimate(queryItem, classAnimate) {
      // Chieu cao cua cua so trinh duyet
-     var windowHeight = $(window).height();
+     const windowHeight = $(window).height();
      // vi tri scroll hien tai cua cua so
-     var scrollPosition = $(window).scrollTop();
-     // animate cho blog details img
-     const imgBlogDetails = $(".blog-container .blog-content .warap-img")
-     const imgBlogDetailEleTop = imgBlogDetails.offset().top
-     if (scrollPosition + windowHeight > imgBlogDetailEleTop) {
-          $(imgBlogDetails).addClass("showItemLeftToRight").removeClass("hidden")
-     }
-     // animate cho content blog details
-     $(".animate-text-1").each(function () {
-          const elementTop = $(this).offset().top;
-          if (scrollPosition + windowHeight > elementTop) {
-               $(this).addClass("showItemLeftToRight").removeClass("hidden")
-          }
-     })
-     // animate cho right blog details
-     $(".animate-item--right").each(function () {
-          const elementTop = $(this).offset().top;
-          var startDuration = 1000; // Thời gian bắt đầu (1s)
-          var durationIncrement = 200; // Bước tăng (0.2s)
-          var currentDuration = startDuration;
-          if (scrollPosition + windowHeight > elementTop) {
+     const scrollPosition = $(window).scrollTop();
+     // gia tri ban dau
+     const startDuration = 1000; // Thời gian bắt đầu (1s)
+     const durationIncrement = 200; // Bước tăng (0.2s)
+     let currentDuration = startDuration; //thoi gian hien tai
+     // vong lap
+     $(queryItem).each(function () {
+          const elementTop = $(this).offset().top; //vi tri cua element tu cua so trinh duyet toi vi tri hien tai
+          if (scrollPosition + windowHeight > elementTop) { //kiem tra phan tu vua di qua cua so duoi cua man hinh
                $(this).css({
-                    'animation-duration': currentDuration / 1000 + 's'
-               }).addClass("showItemRightToLeft").removeClass("hidden")
+                    'animation-duration': currentDuration / 1000 + 's',
+               }).removeClass("hidden").addClass(classAnimate)
                currentDuration += durationIncrement;
+          } else {
+               $(this).addClass("hidden").removeClass(classAnimate)
           }
      })
 }
 
+// page blog 1
+function animatePageBlog1() {
+     // animate cho blog item page blog
+     handleAddAnimate(".blog-item", "showItemDownToUp")
+     // animate cho logo footer
+     handleAddAnimate(".footer-heading .logo", "showItemDownToUp")
+     // animate cho icon social
+     handleAddAnimate(".footer-social li", "showItemDownToUp")
+     // animate footer infor
+     handleAddAnimate(".footer-information ul", "showItemDownToUp")
+     // animate cho send mail footer
+     handleAddAnimate(".footer-sendMail", "showItemDownToUp")
+}
 
-// wait dome
+// page details
+function animatePageBlogDetails() {
+     // left
+     handleAddAnimate(".animate-leftToRight", "showItemLeftToRight")
+     handleAddAnimate(".animate-item--right", "showItemRightToLeft")
+
+     // footer
+     // animate cho logo footer
+     handleAddAnimate(".footer-heading .logo", "showItemDownToUp")
+     // animate cho icon social
+     handleAddAnimate(".footer-social li", "showItemDownToUp")
+     // animate footer infor
+     handleAddAnimate(".footer-information ul", "showItemDownToUp")
+     // animate cho send mail footer
+     handleAddAnimate(".footer-sendMail", "showItemDownToUp")
+}
+// page blog sidebar
+
+function animatePageBlogSidebar() {
+     handleAddAnimate(".animate-leftToRight", "showItemLeftToRight")
+     handleAddAnimate(".animate-item--right", "showItemRightToLeft")
+}
+
+// wait dom
 $(document).ready(function () {
-     // animate showItemDownToUp down to up
+     // get current url
      var currentUrl = window.location.href;
-     if (currentUrl.includes("blog_details.html")) {
-          animateBlogDetails();
-     }
-     // home
-     checkScrollPosition();
-
-     $(window).scroll(function () {
-          if (currentUrl.includes("blog_details.html")) {
-               animateBlogDetails();
+     // run animate
+     function runAnimate() {
+          // dung trang moi show aniamte
+          switch (true) {
+               case currentUrl.includes("blog_details.html"): //page details
+                    animatePageBlogDetails();
+                    break;
+               case currentUrl.includes("blog_sidebar.html"): //page details
+                    animatePageBlogDetails();
+                    break;
+               default: //page home
+                    animatePageBlog1()
+                    break;
           }
-          checkScrollPosition();
+     }
+     // reload page add animate
+     runAnimate()
+     // scroll add animate
+     $(window).scroll(function () {
+          runAnimate()
      });
 
 });
